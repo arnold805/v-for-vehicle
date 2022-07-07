de49913c931a3c629894e01c6d42eaf99aeeed83
Greg <garnold.805@gmail.com>
2022-07-07 15:02:44 -0400

    fixes ReactDOM.render error

--- app/javascript/index.jsx
+++ app/javascript/index.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-import App from './components/app';
+import App from "./components/app";
 
-ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-    document.getElementById("root")
-  );
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
