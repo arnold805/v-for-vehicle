04d83d7bf1edb81093221334900b41902d4c001d
Greg <garnold.805@gmail.com>
2022-07-10 17:46:39 -0400

    refactored httpClient.jsx

--- app/javascript/httpClient.jsx
+++ app/javascript/httpClient.jsx
@@ -1,80 +1,81 @@
 import CsrfToken from "./csrfToken";
 
 export const HttpClient = () => {
-  let responseOkayCallback = function () {
-    throw "Implement Me";
-  };
-
-  let responseNotOkayCallback = function () {
-    throw "Implement Me";
-  };
-
   const headers = {
     "Content-Type": "application/json",
     Accept: "application/json",
     "X-CSRF-Token": CsrfToken(),
   };
 
-  function handleResponse(res) {
+  function handleResponse(res, okCallback, notOkCallback, optionalJsonObject) {
+    if(okCallback === undefined) 
+      okCallback = () => {
+        console.log("The response was okay but nothing was done")
+        // throw("The response was okay but nothing was done")
+      }
+    if(notOkCallback === undefined)
+      notOkCallback = () => { 
+        console.log("The response was not okay and nothing was done")
+        // throw("The response was not okay and nothing was done")
+      }
+
+    if (res.ok) {
+      okCallback(optionalJsonObject);
+    } else {
+      notOkCallback(optionalJsonObject);
+    }
+  }
+
+  function handleResponseWithBody(res, okCallback, notOkCallback) {
     res
       .json()
       .then((json) => {
         console.log(json);
-        if (res.ok) {
-          responseOkayCallback(json);
-        } else {
-          responseNotOkayCallback(json);
-        }
+        handleResponse(res, okCallback, notOkCallback, json)
       })
       .catch((err) => {
-        throw("There was a problem parsing the response body\n" + err);
+        throw "There was a problem parsing the response body\n" + err;
       });
   }
 
-  function handleDeleteResponse(res) {
-    if (res.ok) {
-      responseOkayCallback();
-    } else {
-      responseNotOkayCallback();
-    }
-  }
-
   function catchError(err) {
-    throw("There was a problem connecting to the server" + err);
+    throw "There was a problem connecting to the server" + err;
   }
 
-  function executeRequest(path, options) {
-    fetch(path, options).then(handleResponse).catch(catchError);
+  function executeRequest(path, options, okCallback, notOkCallback) {
+    fetch(path, options)
+      .then((res) => handleResponseWithBody(res, okCallback, notOkCallback))
+      .catch(catchError);
+  }
+
+  function executDeleteRequest(path, options, okCallback, notOkCallback) {
+    fetch(path, options)
+      .then((res) => handleResponse(res, okCallback, notOkCallback))
+      .catch(catchError);
   }
 
   return {
-    get: function (path, responseOkayCallback, responseNotOkayCallback) {
-      this.responseOkayCallback = responseOkayCallback;
-      this.responseNotOkayCallback = responseNotOkayCallback;
+    get: function (path, okCallback, notOkCallback) {
       let options = {
         method: "GET",
         headers,
       };
-      executeRequest(path, options);
+      executeRequest(path, options, okCallback, notOkCallback);
     },
-    post: function (path, obj, responseOkayCallback, responseNotOkayCallback) {
-      this.responseOkayCallback = responseOkayCallback;
-      this.responseNotOkayCallback = responseNotOkayCallback;
+    post: function (path, obj, okCallback, notOkCallback) {
       let options = {
         method: "GET",
         headers,
         body: JSON.stringify(obj),
       };
-      executeRequest(path, options);
+      executeRequest(path, options, okCallback, notOkCallback);
     },
-    delete: function (path, responseOkayCallback, responseNotOkayCallback) {
-      this.responseOkayCallback = responseOkayCallback;
-      this.responseNotOkayCallback = responseNotOkayCallback;
+    delete: function (path, okCallback, notOkCallback) {
       let options = {
         method: "DELETE",
         headers,
       };
-      fetch(path, options).then(handleDeleteResponse).catch(catchError);
+      executDeleteRequest(path, options, okCallback, notOkCallback)
     },
   };
 };
