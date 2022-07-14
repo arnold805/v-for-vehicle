919a82d85d1781b5851fb6aefbb9e6a1f822e6a9
Greg <garnold.805@gmail.com>
2022-07-14 11:49:16 -0400

    updates sell vehicle form again

--- app/javascript/components/SellVehicle.jsx
+++ app/javascript/components/SellVehicle.jsx
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { HttpClient } from "../httpClient";
+import VehicleCard from "./VehicleCard";
 
 const SellVehicle = () => {
   const httpClient = HttpClient();
+  const [newListing, setNewListing] = useState();
   const [formData, setFormData] = useState({
     type: "",
     make: "",
     model: "",
     condition: "",
     price: "",
-    image: "",
     errors: {},
   });
 
@@ -24,63 +25,68 @@ const SellVehicle = () => {
     // checkSellInput(formData);
     event.preventDefault(event);
     console.info("Valid Form");
-    httpClient.post("/api/vehicles", formData);
+    httpClient.post("/api/vehicles", formData, setNewListing);
   }
 
   return (
-    <div className="form-box">
-      <h2>Sell Vehicle</h2>
-      <form className="sell-car-form" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="type"
-          placeholder="type"
-          value={formData.type}
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="make"
-          placeholder="make"
-          value={formData.make}
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="model"
-          placeholder="model"
-          value={formData.model}
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="condition"
-          placeholder="New or Used"
-          value={formData.condition}
-          onChange={handleChange}
-        />
-        <input
-          type="number"
-          name="price"
-          placeholder="price"
-          value={formData.price}
-          onChange={handleChange}
-          step="1000"
-        />
-        <input
-          type="text"
-          name="image"
-          placeholder="image URL"
-          value={formData.image}
-          onChange={handleChange}
-        />
-        <input
-          type="submit"
-          name="add-car-listing-btn"
-          value="Add Vehicle Listing"
-        />
-      </form>
-    </div>
+    <>
+      <div className="form-box">
+        <h2>Sell Vehicle</h2>
+        <form className="sell-car-form" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="type"
+            placeholder="type"
+            value={formData.type}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="make"
+            placeholder="make"
+            value={formData.make}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="model"
+            placeholder="model"
+            value={formData.model}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="condition"
+            placeholder="New or Used"
+            value={formData.condition}
+            onChange={handleChange}
+          />
+          <input
+            type="number"
+            name="price"
+            placeholder="price"
+            value={formData.price}
+            onChange={handleChange}
+            step="1000"
+          />
+          <input
+            type="submit"
+            name="add-car-listing-btn"
+            value="Add Vehicle Listing"
+          />
+        </form>
+      </div>
+      <br />
+      <br />
+      {newListing ? (
+        <>
+          <h2>Here is your new listing:</h2>
+          <VehicleCard vehicle={newListing} />{" "}
+        </>
+      ) : (
+        <></>
+      )}
+    </>
   );
 };
 
