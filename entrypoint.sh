2ffcd075638f6889acaec1008a1292c60d8dadc0
Greg <garnold.805@gmail.com>
2022-09-30 17:06:41 -0400

    moves migration script from docker-compose.yml to entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,8 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /v-for-vehicle/tmp/pids/server.pid
 
+# Run the migrations
+RAILS_ENV=$RAILS_ENV bundle exec rails db:create db:migrate
+
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
